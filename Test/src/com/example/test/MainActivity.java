4fb2095dcec47025bc745aec8e8f0beaa302672d
Lilit <lilit@lilit-Aspire-E1-571G.(none)>
2013-12-20 15:43:56 +0400

    developed start and stop button click

--- Test/src/com/example/test/MainActivity.java
+++ Test/src/com/example/test/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.timer;
 import java.util.Timer;
 import java.util.TimerTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.app.Activity;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -15,6 +17,7 @@ public class MainActivity extends Activity {
 	Button mStart;
 	Button mStop;
 	int count = 1000;
+	TimerTask timerTask;	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -25,39 +28,45 @@ public class MainActivity extends Activity {
 		mStart = (Button) findViewById(R.id.start_btn);
 		mStop = (Button) findViewById(R.id.stop_btn);
 
-		final TimerTask Task = new TimerTask() {
-			@Override
-			public void run() {
-				runOnUiThread(new Runnable() {
-					@Override
-					public void run() {
-						if (count == 0) {
-							cancel();
-						}
-						mText.setText(String.valueOf(count));
-						count--;
-					}
-				});
-			}
-		};
-
-		Timer countdown = new Timer();
-		countdown.schedule(Task, 0, 1000);
-
 		mStart.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View arg0) {
-				
-					Task.run();
+				doTimerTask();
 			}
 		});
 
 		mStop.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				Task.cancel();
+				stopTask();
 			}
 		});
 
 	}
+	public void doTimerTask(){
+		final Handler handler = new Handler();
+		timerTask = new TimerTask() {
+    	        public void run() {
+    	                handler.post(new Runnable() {
+    	                        public void run() {
+    	                        	if (count == 0) {
+    	    							cancel();
+    	    						} else {
+    	    						mText.setText(String.valueOf(count));
+    	    						count--;
+    	                        }
+    	                        }
+    	               });
+    	        }};
+    	        Timer t = new Timer();
+    	    t.schedule(timerTask, 0, 1000);
+ 
+    	 }
+	  public void stopTask(){
+		  
+   	   if(timerTask!=null){
+   	      mText.setText(String.valueOf(count));
+   	      timerTask.cancel();
+   	 }
+}
 }
